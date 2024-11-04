67e0933be401542132c024305dac6f8d991668d4
AK <akrivit@gmail.com>
2024-11-03 21:36:57 -0500

    progress

--- components/extract.js
+++ components/extract.js
@@ -19,21 +19,28 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 
 
+/** Utility function for caching */
+async function getCachedData(filename, fetchData, sliceLimit = 10000) {
+	try {
+		let cachedData = await u.load(`${TEMP_DIR}/${filename}`, true);
+		if (NODE_ENV === 'dev') console.log(`Loaded cached ${filename}`);
+		if (NODE_ENV === 'dev') cachedData = cachedData.slice(0, sliceLimit);
+		return cachedData;
+	} catch (e) {
+		if (NODE_ENV === 'dev') debugger;
+		// Proceed to fetch data if cache loading fails
+		const data = await fetchData();
+		cachedData = await u.touch(`${TEMP_DIR}/${filename}`, data, true);
+		if (NODE_ENV === 'dev') console.log(`Wrote ${filename} to cache`);
+		return cachedData;
+	}
+}
+
 /** Fetching Shows */
 export async function getShows() {
-	try {
-		let cachedShows = null;
-		try {
-			cachedShows = await u.load(`${TEMP_DIR}/shows.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached shows");
-			return cachedShows;
-		} catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
-
+	return getCachedData('shows.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching shows from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getShowOptions = {
 			method: 'GET',
 			url: 'https://api.phish.net/v5/shows/artist/phish.json',
@@ -43,34 +50,18 @@ export async function getShows() {
 
 		const shows = await fetch(getShowOptions);
 		if (!shows?.data) throw new Error("No data returned from phish.net");
-		cachedShows = await u.touch(`${TEMP_DIR}/shows.json`, shows.data, true);
-		if (NODE_ENV === 'dev') console.log("wrote shows to cache");
-		return cachedShows;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return shows.data;
+	});
 }
 
-/** Fetching Setlists */
+/** Fetching Performances */
 export async function getPerformances() {
-	try {
-		let cachedPerformances = null;
-		try {
-			cachedPerformances = await u.load(`${TEMP_DIR}/performances.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached performances");
-			return cachedPerformances;
-		}
-		catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
+	return getCachedData('performances.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching performances from phish.net");
 		const shows = await getShows();
 		const pastShows = shows.filter(s => dayjs(s.showdate).isBefore(dayjs()));
 		const showDates = pastShows.map(s => s.showdate);
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getPerformancesOptions = {
 			concurrency: 10,
 			delay: 1000,
@@ -85,79 +76,42 @@ export async function getPerformances() {
 			}
 		};
 
-		const getSetlistRequests = showDates.map(date => {
-			const url = `${getPerformancesOptions.url}${date}.json`;
-			return { ...getPerformancesOptions, url };
-		});
+		const getSetlistRequests = showDates.map(date => ({
+			...getPerformancesOptions,
+			url: `${getPerformancesOptions.url}${date}.json`
+		}));
 
 		const setlists = await fetch(getSetlistRequests);
-		const performanceData = setlists.map(s => s.data);
-		cachedPerformances = await u.touch(`${TEMP_DIR}/performances.json`, performanceData.flat(), true);
-		if (NODE_ENV === 'dev') console.log("wrote setlists to cache");
-		return cachedPerformances;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return setlists.map(s => s.data).flat();
+	});
 }
 
+/** Fetching Venues */
 export async function getVenues() {
-	try {
-		let cachedVenues = null;
-		try {
-			cachedVenues = await u.load(`${TEMP_DIR}/venues.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached vanues");
-			return cachedVenues;
-		}
-		catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
+	return getCachedData('venues.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching venues from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getVenueOptions = {
-			concurrency: 10,
-			delay: 1000,
-			verbose: false,
 			method: 'GET',
 			url: 'https://api.phish.net/v5/venues',
 			searchParams: { apikey: API_KEY },
-			headers: { 'User-Agent': 'mixpanel-phish' },
-			noBatch: true,
-			errorHandler: (e) => {
-				if (NODE_ENV === 'dev') debugger;
-			}
+			headers: { 'User-Agent': 'mixpanel-phish' }
 		};
 
-
 		const venues = await fetch(getVenueOptions);
-		const venueData = venues?.data || [];
-		cachedVenues = await u.touch(`${TEMP_DIR}/venues.json`, venueData, true);
-		if (NODE_ENV === 'dev') console.log("wrote venues to cache");
-		return cachedVenues;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return venues?.data || [];
+	});
 }
 
-
+/** Fetching Attendance */
 export async function getAttendance() {
-	try {
-		let cachedAttendance = null;
-		try {
-			cachedAttendance = await u.load(`${TEMP_DIR}/attendance.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached attendance");
-			return cachedAttendance;
-		}
-		catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
+	return getCachedData('attendance.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching attendance from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
+		const shows = await getShows();
+		const pastShows = shows.filter(s => dayjs(s.showdate).isBefore(dayjs()));
+		const showIds = pastShows.map(s => s.showid);
+
 		const getAttendanceOptions = {
 			concurrency: 25,
 			delay: 1000,
@@ -169,57 +123,29 @@ export async function getAttendance() {
 			noBatch: true,
 			errorHandler: (e) => {
 				if (NODE_ENV === 'dev') debugger;
-			},
-			responseHandler: (r) => {
-				const resp = r.data.map(person => {
-					return {
-						uid: person.uid,
-						username: person.username,
-						venue: person.venue,
-						showid: person.showid,
-						venueid: person.venueid,
-						date: person.showdate
-					};
-				});
-				return resp;
-
 			}
 		};
+
+		const getAttendanceRequests = showIds.map(id => ({
+			...getAttendanceOptions,
+			url: `${getAttendanceOptions.url}${id}.json`
+		}));
+
+		const attendanceData = await fetch(getAttendanceRequests);
+		return attendanceData.flat();
+	});
+}
+
+/** Fetching Reviews */
+export async function getReviews() {
+	return getCachedData('reviews.json', async () => {
+		if (NODE_ENV === 'dev') console.log("Fetching reviews from phish.net");
+
 		const shows = await getShows();
 		const pastShows = shows.filter(s => dayjs(s.showdate).isBefore(dayjs()));
 		const showIds = pastShows.map(s => s.showid);
-		const getAttendanceRequests = showIds.map(id => {
-			const url = `${getAttendanceOptions.url}${id}.json`;
-			return { ...getAttendanceOptions, url };
-		});
 
-		const attendanceData = await fetch(getAttendanceRequests);		
-		cachedAttendance = await u.touch(`${TEMP_DIR}/attendance.json`, attendanceData.flat(), true);
-		if (NODE_ENV === 'dev') console.log("wrote attendance to cache");
-		return cachedAttendance;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
-}
-
-
-export async function getReviews() {
-	try {
-		let cachedReviews = null;
-		try {
-			cachedReviews = await u.load(`${TEMP_DIR}/reviews.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached reviews");
-			return cachedReviews;
-		}
-		catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
-		if (NODE_ENV === 'dev') console.log("Fetching reviews from phish.net");
-
-		/** @type {fetch.BatchRequestConfig} */
-		const getAttendanceOptions = {
+		const getReviewOptions = {
 			concurrency: 25,
 			delay: 1000,
 			verbose: false,
@@ -230,59 +156,24 @@ export async function getReviews() {
 			noBatch: true,
 			errorHandler: (e) => {
 				if (NODE_ENV === 'dev') debugger;
-			},
-			responseHandler: (r) => {				
-				const resp = r.data.map(review => {
-					return {
-						uid: review.uid,
-						username: review.username,
-						venue: review.venue,
-						showid: review.showid,
-						venueid: review.venueid,
-						date: review.showdate,
-						score: review.score,
-						text: review.review_text,
-						review_id: review.reviewid						
-					};
-				});
-				return resp;
-
 			}
 		};
-		const shows = await getShows();
-		const pastShows = shows.filter(s => dayjs(s.showdate).isBefore(dayjs()));
-		const showIds = pastShows.map(s => s.showid);
-		const getReviewRequests = showIds.map(id => {
-			const url = `${getAttendanceOptions.url}${id}.json`;
-			return { ...getAttendanceOptions, url };
-		});
 
-		const reviewData = await fetch(getReviewRequests);		
-		cachedReviews = await u.touch(`${TEMP_DIR}/reviews.json`, reviewData.flat(), true);
-		if (NODE_ENV === 'dev') console.log("wrote reviews to cache");
-		return cachedReviews;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		const getReviewRequests = showIds.map(id => ({
+			...getReviewOptions,
+			url: `${getReviewOptions.url}${id}.json`
+		}));
+
+		const reviewData = await fetch(getReviewRequests);
+		return reviewData.flat();
+	});
 }
 
-
+/** Fetching Songs */
 export async function getSongs() {
-	try {
-		let cachedSongs = null;
-		try {
-			cachedSongs = await u.load(`${TEMP_DIR}/songs.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached songs");
-			return cachedSongs;
-		} catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
-
+	return getCachedData('songs.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching songs from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getSongOptions = {
 			method: 'GET',
 			url: 'https://api.phish.net/v5/songs/',
@@ -292,30 +183,15 @@ export async function getSongs() {
 
 		const songs = await fetch(getSongOptions);
 		if (!songs?.data) throw new Error("No data returned from phish.net");
-		cachedSongs = await u.touch(`${TEMP_DIR}/songs.json`, songs.data, true);
-		if (NODE_ENV === 'dev') console.log("wrote songs to cache");
-		return cachedSongs;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return songs.data;
+	});
 }
 
+/** Fetching Jam Notes */
 export async function getJamNotes() {
-	try {
-		let cachedJamNotes = null;
-		try {
-			cachedJamNotes = await u.load(`${TEMP_DIR}/jam-notes.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached jam notes");
-			return cachedJamNotes;
-		} catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
-
+	return getCachedData('jam-notes.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching jam notes from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getJamNoteOptions = {
 			method: 'GET',
 			url: 'https://api.phish.net/v5/jamcharts/',
@@ -325,31 +201,15 @@ export async function getJamNotes() {
 
 		const jamNotes = await fetch(getJamNoteOptions);
 		if (!jamNotes?.data) throw new Error("No data returned from phish.net");
-		cachedJamNotes = await u.touch(`${TEMP_DIR}/jam-notes.json`, jamNotes.data, true);
-		if (NODE_ENV === 'dev') console.log("wrote songs to cache");
-		return cachedJamNotes;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return jamNotes.data;
+	});
 }
 
-
+/** Fetching Users */
 export async function getUsers() {
-	try {
-		let cachedUsers = null;
-		try {
-			cachedUsers = await u.load(`${TEMP_DIR}/users.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached users");
-			return cachedUsers;
-		} catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
-
+	return getCachedData('users.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching users from phish.net");
 
-		/** @type {fetch.BatchRequestConfig} */
 		const getUserOptions = {
 			method: 'GET',
 			url: 'https://api.phish.net/v5/users/uid/0.json',
@@ -359,61 +219,41 @@ export async function getUsers() {
 
 		const users = await fetch(getUserOptions);
 		if (!users?.data) throw new Error("No data returned from phish.net");
-		cachedUsers = await u.touch(`${TEMP_DIR}/users.json`, users.data, true);
-		if (NODE_ENV === 'dev') console.log("wrote users to cache");
-		return cachedUsers;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		return users.data;
+	});
 }
 
+/** Fetching Performance Metadata */
 export async function getPerformanceMeta() {
-	try {
-		let cachedPerfMeta = null;
-		try {
-			cachedPerfMeta = await u.load(`${TEMP_DIR}/performance-metadata.json`, true);
-			if (NODE_ENV === 'dev') console.log("Loaded cached performance metadata");
-			return cachedPerfMeta;
-		}
-		catch (e) {
-			if (NODE_ENV === 'dev') debugger;
-		}
+	return getCachedData('performance-metadata.json', async () => {
 		if (NODE_ENV === 'dev') console.log("Fetching performance metadata from phish.in");
+
 		const shows = await getShows();
 		const pastShows = shows.filter(s => dayjs(s.showdate).isBefore(dayjs()));
 		const showDates = pastShows.map(s => s.showdate);
 
-		/** @type {fetch.BatchRequestConfig} */
-		const getPerformancesOptions = {
-			concurrency: 25,
-			delay: 1000,
+		const getPerformanceMetaOptions = {
+			concurrency: 10,
+			delay: 2500,
 			verbose: false,
 			method: 'GET',
 			url: 'https://phish.in/api/v2/shows/',
 			headers: { 'User-Agent': 'mixpanel-phish' },
 			noBatch: true,
 			errorHandler: (e) => {
-				if (e.status === 404) return;
+				if (e.status === 404) return {};
 				if (NODE_ENV === 'dev') debugger;
 			}
 		};
 
-		const getPerfMetaReqs = showDates.map(date => {
-			const url = `${getPerformancesOptions.url}${date}.json`;
-			return { ...getPerformancesOptions, url };
-		});
+		const getPerformanceMetaRequests = showDates.map(date => ({
+			...getPerformanceMetaOptions,
+			url: `${getPerformanceMetaOptions.url}${date}.json`
+		}));
 
-		const performanceData = await fetch(getPerfMetaReqs);		
-		cachedPerfMeta = await u.touch(`${TEMP_DIR}/performance-metadata.json`, performanceData, true);
-		if (NODE_ENV === 'dev') console.log("wrote performance metadata to cache");
-		return cachedPerfMeta;
-	}
-	catch (e) {
-		if (NODE_ENV === 'dev') debugger;
-		throw e;
-	}
+		const performanceData = await fetch(getPerformanceMetaRequests);
+		return performanceData;
+	});
 }
 
 
--- components/transform.js
+++ components/transform.js
@@ -11,20 +11,160 @@ import {
 	getReviews,
 	getShows,
 	getSongs,
-	getVenues
+	getVenues,
+	getUsers
 } from './extract.js';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 
-export async function phanProfiles() {
+/**
+ * EVENTS
+ */
+
+export async function attendanceEvents() {
+
+	const attendance = await getAttendance();
+	const shows = await getShows();
+	const performances = await getPerformances();
+	const songBank = await getSongs();
+	const venues = await getVenues();
+	const meta = await getPerformanceMeta();
+	const events = [];
+
+	attendance.forEach(u => {
+		try {
+			const distinct_id = u.uid;
+			const username = u.username;
+			const show = shows.find(s => s.showid === u.showid);
+			const venue = venues.find(v => v.venueid === show.venueid);
+			const songsHeard = performances.filter(p => p.showid === u.showid);
+			const time = dayjs.utc(show.showdate).toISOString();
+			const metaData = meta.find(m => dayjs.utc(m?.date).toISOString() === time);
+
+			const attendanceEvent = {
+				event: "attended show",
+				distinct_id,
+				username,
+				time,
+
+				//group analytics
+				show_id: show.showid,
+				venue_id: venue.venueid,
+
+				latitude: metaData?.venue?.latitude,
+				longitude: metaData?.venue?.longitude,
+				duration_mins: Math.floor(metaData?.duration / 60000),
+			};
+
+			events.push(attendanceEvent);
+
+			songsHeard.forEach(s => {
+				try {
+					const song = songBank.find(sb => sb.songid === s.songid);
+					const songMeta = metaData?.tracks?.find(m => m?.slug === song?.slug);
+					const songEvent = {
+						event: "heard song",
+						distinct_id,
+						username,
+						time,
+
+						song_id: s?.songid,
+						venue_id: venue?.venueid,
+						show_id: show?.showid,
+						performance_id: `${show.showid}-${s.set}-${s.position}-${s.songid}`,
+						song_name: song?.song,
+						duration_mins: Math.floor(songMeta?.duration / 60000),
+						url: songMeta?.mp3_url,
+
+					};
+					events.push(songEvent);
+				}
+				catch (e) {
+					if (NODE_ENV === 'dev') debugger;
+				}
+			});
+		}
+
+		catch (e) {
+			if (NODE_ENV === 'dev') debugger;
+		}
+	});
+
+	return events;
 }
 
-export async function attendanceEvents() { }
+export async function reviewEvents() {
+	const reviews = await getReviews();
+	const shows = await getShows();
+	const venues = await getVenues();
+	const meta = await getPerformanceMeta();
 
-export async function reviewEvents() { }
+	const events = [];
 
-export async function songProfiles() { }
+	reviews.forEach(r => {
+		try {
+			const time = dayjs.utc(r.date).toISOString();
+			const distinct_id = r.uid;
+			const username = r.username;
+			const show = shows.find(s => s.showid === r.showid);
+			const metaData = meta.find(m => dayjs.utc(m?.date).toISOString() === time);
+			const reviewEvent = {
+				event: "reviewed show",
+				distinct_id,
+				username,
+				time,
+				duration_mins: Math.floor(metaData?.duration / 60000),
+				show_id: r.showid,
+				venue_id: show.venueid,
+				review_id: r.review_id,
+				rating: r.score,
+				review: r.text,
+				latitude: metaData?.venue?.latitude,
+				longitude: metaData?.venue?.longitude,
+
+			};
+			events.push(reviewEvent);
+		}
+		catch (e) {
+			if (NODE_ENV === 'dev') debugger;
+		}
+	});
+	return events;
+}
+
+export async function performanceEvents() { }
+
+
+
+
+/**
+ * PROFILES
+ */
+
+export async function phanProfiles() {
+
+	const users = await getUsers();
+	const modeledUsers = users.map(u => {
+		try {
+			const distinct_id = u.uid;
+			const email = u.username;
+			const created = dayjs.utc(u.date_joined).toISOString();
+			return {
+				distinct_id,
+				email,
+				created,
+				...u
+			};
+		}
+		catch (e) {
+			if (NODE_ENV === 'dev') debugger;
+			return {};
+		}
+	});
+	return modeledUsers;
+
+}
 
 export async function performanceProfiles() {
 	try {
@@ -109,11 +249,45 @@ export async function performanceProfiles() {
 	}
 }
 
-export async function performanceEvents() { }
+export async function venueProfiles() { }
+
+export async function songProfiles() {
+	try {
+		const songs = await getSongs();
+		const profiles = songs.map(s => {
+			const distinct_id = s.songid;
+			const topName = s.song;
+			const bottomName = s.artist;
+			const profile = {
+				distinct_id,
+				$name: topName,
+				$email: bottomName,
+
+
+				DEBUT: s.debut,
+				TIMES_PLAYED: s.times_played,
+				LAST_PLAYED: s.last_played
+			};
+			return profile;
+		});
+		return profiles;
+	} catch (e) {
+		if (NODE_ENV === 'dev') debugger;
+		throw e;
+	}
+}
+
+
+
+
 
 
 if (import.meta.url === new URL(`file://${process.argv[1]}`).href) {
 	const p = await performanceProfiles();
-	const uniq = Array.from(new Set(p.map(a => a.ERA)));
+	const u = await phanProfiles();
+	const a = await attendanceEvents();
+	const r = await reviewEvents();
+	const s = await songProfiles();
+	const pp = await performanceEvents();
 	if (NODE_ENV === 'dev') debugger;
 }
