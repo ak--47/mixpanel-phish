82ea3a55c005a564679ec971d2b549b52d96d5bf
AK <akrivit@gmail.com>
2024-12-03 23:48:29 -0500

    cleanup

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -44,3 +44,6 @@ total_duration=$((total_end_time - total_start_time))
 
 echo -e "\n${GREEN}ETL pipeline completed successfully${NC}\n"
 echo -e "\n${GREEN}Total duration: $total_duration seconds${NC}\n"
+
+rm -rf ./tmp/*
+ 
